18c2364505e49b408b1b097232ad933489268651
Carlos Domani <carlos.domani@torchtechnologies.com>
2025-10-29 14:29:13 -0500

    Update gitsigns keybinds

--- nvim/.config/nvim/lua/kickstart/plugins/gitsigns.lua
+++ nvim/.config/nvim/lua/kickstart/plugins/gitsigns.lua
@@ -24,7 +24,7 @@ return {
         end
 
         -- Navigation
-        map('n', ']c', function()
+        map('n', '<PageDown>', function()
           if vim.wo.diff then
             vim.cmd.normal { ']c', bang = true }
           else
@@ -32,7 +32,7 @@ return {
           end
         end, { desc = 'Jump to next git [c]hange' })
 
-        map('n', '[c', function()
+        map('n', '<PageUp>', function()
           if vim.wo.diff then
             vim.cmd.normal { '[c', bang = true }
           else
