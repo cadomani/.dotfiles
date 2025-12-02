4130e90ab6c7e258a8d658753af1e4331cd2d338
Carlos Domani <carlos.a.domani@gmail.com>
2025-12-01 23:22:37 -0600

    Add options.lua overrides

--- nvim/.config/nvim/lua/custom/options.lua
+++ nvim/.config/nvim/lua/custom/options.lua
@@ -1,11 +1,12 @@
 -- Custom options overrides
 -- These are applied AFTER kickstart's options.lua
+--
+-- NOTE: Some vim.g settings (have_nerd_font, lspconfig_suppress_deprecation_warnings)
+-- must be set in init.lua BEFORE plugins load, so they live there instead.
 
--- Set shell to Homebrew zsh
-vim.o.shell = '/opt/homebrew/bin/zsh'
-
--- Enable Nerd Font icons (using CaskaydiaMono Nerd Font)
-vim.g.have_nerd_font = true
-
--- Suppress lspconfig deprecation warning until upstream updates to vim.lsp.config
-vim.g.lspconfig_suppress_deprecation_warnings = true
+-- Set shell to zsh (platform-specific path)
+if vim.fn.has 'mac' == 1 then
+  vim.o.shell = '/opt/homebrew/bin/zsh'
+else
+  vim.o.shell = '/usr/bin/zsh'
+end
