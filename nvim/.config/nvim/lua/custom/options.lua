c60eab2e533522c570403dfbe83963ff13c9c1d5
Carlos Domani <carlos.a.domani@gmail.com>
2025-12-01 22:46:53 -0600

    Enable Nerd Font icons

--- nvim/.config/nvim/lua/custom/options.lua
+++ nvim/.config/nvim/lua/custom/options.lua
@@ -4,5 +4,8 @@
 -- Set shell to Homebrew zsh
 vim.o.shell = '/opt/homebrew/bin/zsh'
 
+-- Enable Nerd Font icons (using CaskaydiaMono Nerd Font)
+vim.g.have_nerd_font = true
+
 -- Suppress lspconfig deprecation warning until upstream updates to vim.lsp.config
 vim.g.lspconfig_suppress_deprecation_warnings = true
