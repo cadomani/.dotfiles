ea4dc70d66fc7df411f9aa7edeb1c0828f73c313
Carlos Domani <carlos.a.domani@gmail.com>
2025-12-01 22:40:54 -0600

    Suppress lspconfig deprecation warning

--- nvim/.config/nvim/lua/custom/options.lua
+++ nvim/.config/nvim/lua/custom/options.lua
@@ -3,3 +3,6 @@
 
 -- Set shell to Homebrew zsh
 vim.o.shell = '/opt/homebrew/bin/zsh'
+
+-- Suppress lspconfig deprecation warning until upstream updates to vim.lsp.config
+vim.g.lspconfig_suppress_deprecation_warnings = true
